63d1f234ec8d412d6b78ead6c9aadb5059ca5b41
LeoComp <levstelman@mail.ru>
2020-03-12 16:50:17 +0300

    fix

--- Lab04/Lab04.cpp
+++ Lab04/Lab04.cpp
@@ -6,6 +6,11 @@ struct time { int hours = 0, minutes = 0, seconds = 0; };
 long hms_to_secs(int hours, int min, int sec);
 long time_to_secs(time t1, time t2);
 time secs_to_time(long totalSec);
+double power(double n, int p = 2); //по умолчанию любое число возводим во вторую степень
+char power(char n, int p);
+int power(int n, int p);
+long power(long n, int p);
+float power(float n, int p);
 
 int main()
 {
@@ -18,7 +23,7 @@ int main()
     {
         int hours, min, sec;
         cout << "Введите время в удобном для вас формате(например, 12 34 58)\n" && cin >> hours >> symbol >> min >> symbol >> sec;
-        cout << "Всего секунд: " << hms_to_secs(hours, min, sec) << "\nПродолжить вычисления?(введите 'y' если согласны, или любой другой символ, дабы отказаться)\t" && cin >> prodolzhenie;
+        cout << "Всего секунд: " << hms_to_secs(hours, min, sec) << "\nПродолжить вычисления?(введите 'y' если согласны, или любой другой символ, дабы отказаться)\n" && cin >> prodolzhenie;
     }
 
     //6
@@ -27,16 +32,56 @@ int main()
         Первая, time_to_secs(), принимает в качестве аргумента значение типа time и возвращает эквивалентное значение в секундах типа long.
         Вторая, secs_to_time(), в качестве аргумента принимает число секунд, имеющее тип long, а возвращает эквивалентное значение типа time.   */
     time setTime1, setTime2;
-    cout << "Введите первое значение времени в формате 12:59:59" << endl;
+    cout << "\nВведите первое значение времени в формате 12:59:59" << endl;
     cin >> setTime1.hours >> symbol >> setTime1.minutes >> symbol >> setTime1.seconds;
     cout << "Введите второе значение времени в формате 12:59:59" << endl;
     cin >> setTime2.hours >> symbol >> setTime2.minutes >> symbol >> setTime2.seconds;
     long totalSec = time_to_secs(setTime1, setTime2);
-    cout << "Сумма времени " << secs_to_time(totalSec).hours << secs_to_time(totalSec).minutes << secs_to_time(totalSec).seconds << endl << endl;
+    cout << "Сумма времени " << secs_to_time(totalSec).hours << ':' << secs_to_time(totalSec).minutes << ':' << secs_to_time(totalSec).seconds << endl << endl;
 
     //7
     /*  Взяв в качестве основы функцию power() из упражнения 2, работающую только со значением типа double, создайте перегруженные функции с этим же именем, 
-        принимающими в качестве аргумента значения типа char, int, long и float. Напишите программу, вызывающую функцию power() со всеми возможными типами аргументов. */
+        принимающими в качестве аргумента значения типа char, int, long и float. Напишите программу, вызывающую функцию power() со всеми возможными типами аргументов. 
+
+        Возведение числа n в степень р — это умножение числа n на себя р раз. Напишите функцию с именем power(), которая в качестве аргументов принимает значение типа double для n
+        и значение типа int для р и возвращает значение типа double. Для аргумента, соответствующего степени числа, задайте значение по умолчанию, равное 2, 
+        чтобы при отсутствии показателя степени при вызове функции число n возводилось в квадрат.
+        Напишите функцию main(), которая запрашивает у пользователя ввод аргументов для функции power(), и отобразите на экране результаты ее работы. */
+    cout << "Какой тип данных планируете возводить в степень? Введите первую букву названия типа (int, long, float, double, char).\n" && cin >> symbol;
+    cout << "Введите значение: ";
+    switch (symbol)
+    {
+        int i, stepen; long l; float f; double d; char c;
+    case 'i':
+        cin >> i;
+        cout << "Возвести в степень " && cin >> stepen;
+        cout << "Ответ: " << power(i, stepen) << endl << endl;
+        break;
+    case 'l':
+        cin >> l;
+        cout << "Возвести в степень " && cin >> stepen;
+        cout << "Ответ: " << power(l, stepen) << endl << endl;
+        break;
+    case 'f':
+        cin >> f;
+        cout << "Возвести в степень " && cin >> stepen;
+        cout << "Ответ: " << power(f, stepen) << endl << endl;
+        break;
+    case 'd':
+        cin >> d;
+        cout << "Возвести в степень " && cin >> stepen;
+        cout << "Ответ: " << power(d, stepen) << endl << endl;
+        break;
+    case 'c':
+        cin >> c;
+        cout << "Возвести в степень " && cin >> stepen;
+        cout << "Ответ: " << power(c, stepen) << endl << endl;
+        break;
+    default:
+        cout << "Ошибка ввода" << endl << endl;
+        break;
+    }
+
 
 }
 
@@ -55,4 +100,40 @@ time secs_to_time(long totalSec)
     stt.minutes = (totalSec % 3600) / 60;
     stt.seconds = (totalSec % 3600) % 60;
     return stt;
+}
+
+double power(double n, int p)
+{
+    double result = 1; //начать с 1
+    for (int j = 0; j < p; j++) //умножить на n
+        result *= n; //p раз
+    return result;
+}
+char power(char n, int p)
+{
+    char result = 1; 
+    for (int j = 0; j < p; j++) 
+        result *= n; 
+    return result;
+}
+int power(int n, int p)
+{
+    int result = 1;
+    for (int j = 0; j < p; j++) 
+        result *= n;
+    return result;
+}
+long power(long n, int p)
+{
+    long result = 1; 
+    for (int j = 0; j < p; j++) 
+        result *= n; 
+    return result;
+}
+float power(float n, int p)
+{
+    float result = 1; 
+    for (int j = 0; j < p; j++) 
+        result *= n; 
+    return result;
 }
